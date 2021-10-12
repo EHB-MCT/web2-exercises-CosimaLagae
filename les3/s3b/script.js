41697232a93144e068a42049deab0bc694e0f7bb
CosimaLagae <cosima.lagae@student.ehb.be>
2021-10-12 12:37:05 +0200

    show me something

--- les3/s3b/script.js
+++ les3/s3b/script.js
@@ -1,32 +1,57 @@
 "use strict!"
 
+let baseurl = "http://www.omdbapi.com/?apikey=c4255158&";
 
-async function getData(){
-    let response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c4255158');
-    console.log("response");
+
+window.onload = function(){
+    console.log("loaded");
+
+    let url = baseurl + 't=parasite';
+
+    getData(url).then(result => {
+        console.log(result);
+    });
+
+    setTimeout(buildCard, 3000);
+
+    function  buildCard(){
+        let html = `
+            <div class="card-body">
+                <h5 class="card-title">${url.Title}</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>`
+        document.getElementById('card-body').innerHTML=html;
+    }
+}
+
+async function getData(url){
+    let response = await fetch(url);
     return await response.json();
 }
 
 
-/*window.onload = function(){
-    getData();
-    setTimeout(buildList, 3000);
 
-    function  buildList(){
-        let html = "";
-        for(let p of pokemon){
-            html += 
-            `<div id="list">
-            <img src="${p.sprites.front_default}" alt="">
-            <div >
-            <h1>${p.name}</h1>
-            <h2>${p.types}</h2>
-            <button>Add to team</button>
-            </div>`
-        }
-        document.getElementById('list').innerHTML=html;
-    }
 
-    let team1 = new Team();
+
+// c4255158
+//10531ad7&
+
+
+
+
+
+/*async function getData(){
+    let response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c4255158');
+    return await response.json();
+}
+
+getData().then(data => {
+    console.log(data);
+})
+
+window.onload = function(){
+   document.getElementById('movieField').value 
+ 
 
 }*/
